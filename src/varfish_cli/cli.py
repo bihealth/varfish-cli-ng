733549be38c7482eaa9b0ab376232a895bc9ab81
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-10-27 16:07:14 +0200

    feat: add entry point for command line interface

--- src/varfish_cli/cli.py
+++ src/varfish_cli/cli.py
@@ -1,10 +1,24 @@
-import sys
+import logging
+
+import click
+import logzero
+
+from varfish_cli import _version
 
 
-def main():
-    print("Hello VarFish TNG")
-    return 0
+@click.group()
+@click.version_option(_version.__version__)
+@click.option("--verbose/--no-verbose", default=False, help="Enable/disable logging verbosity.")
+@click.pass_context
+def cli_main(ctx: click.Context, verbose: bool):
+    """Main entry point for CLI via click."""
+    ctx.ensure_object(dict)
+    ctx.obj["verbose"] = verbose
+    if verbose:
+        logzero.loglevel(logging.DEBUG)
+    else:
+        logzero.loglevel(logging.INFO)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    cli_main()
